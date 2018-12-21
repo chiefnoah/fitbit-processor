3cfa7711dc430d75fa9a949d690329d1e63d90f0
Noah Pederson <pedersonnoah@gmail.com>
2018-12-21 00:04:59 -0600

    Adds some basic structure

---
+++ pkg/web/web.go
@@ -0,0 +1,10 @@
+package web
+
+import (
+	"net/http"
+)
+
+//HandleFitbitNotification handles a notification event from Fitbit
+func HandleFitbitNotification(w http.ResponseWriter, r *http.Request) {
+
+}
